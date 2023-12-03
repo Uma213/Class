b48aae6a398992e005142ccdc436b3af9cc246f1
Uma Pathapalyam <150056525+Uma213@users.noreply.github.com>
2023-12-03 13:52:33 +0530

    Add files via upload

---
+++ python.class.py
@@ -0,0 +1,24 @@
+#create an empt mpty class Student
+class Student:
+definit init__(self,name, age, graduate):
+→→self.n elf.name name
+self. elf.age-age
+se self.graduate=graduate
+#create an object Stud_1 of the class
+name_1=input ("sl name: ")
+age_1=input("s1 ("s1 age: ")
+graduate_1=inpu ("s1 degree: ")
+Stud_1=Student Student (name_1, age_1, graduate_1)
+#take three inputs name, age, degree and assign the inputs to the stud_1 object
+name_2-in input("s2 name: ")
+age_2=in input ("s2 age: ") graduate uate_2=input("s2 degree: ")
+#create an object Stud_2 of the class
+Stud_2=Student (name_2, age_2,graduate_2)
+#take three inputs name, age, degree and assign the inputs to the stud_2 onject
+#printing the details of the two students
+print("Stud_1.name:", Stud_1.name)
+print("Stud_1.age:", Stud_1.age)
+print("Stud_1.graduate:", Stud_1.graduate)
+print("Stud_2.name:", Stud_2.name)
+print("Stud_2.age:", Stud_2.age)
+print("Stud_2.graduate:", Stud_2.graduate)
